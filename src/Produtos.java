12394dbc8a27fe94709dc92cac02ad76f49708f3
2222000271 <diasemillim2006@gmail.com>
2026-04-14 20:18:37 -0300

    Emcapsulamento parado em telefone em Usuários;
    Criado add estoque

--- src/Produtos.java
+++ src/Produtos.java
@@ -3,20 +3,57 @@ public class Produtos {
     public String nomeProduto;
     public String descricao;
     protected double preco;
-    public Categoria categoria;
+    private Categoria categoria;
+    private int estoque;
 
-    public Produtos(int idProduto, String nomeProduto, String descricao, double preco, Categoria categoria){
+    public Produtos(int idProduto, String nomeProduto, String descricao, double preco, Categoria categoria, int estoque){
        this.idProduto = idProduto;
        this.nomeProduto = nomeProduto;
        this.descricao = descricao;
        this.preco = preco;
        this.categoria = categoria;
+       this.estoque = estoque;
     }
     public double getPreco(){
         return preco;
     }
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
 
     public String getNomeProduto(){
         return nomeProduto;
+    }
+    public void setNome(String nome) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public Categoria getCategoria(){
+        return categoria;
+    }
+    public void setCategoria(Categoria categoria){
+        this.categoria = categoria;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+
+    public void reduzirEstoque(int quantidade){
+        if(quantidade <= estoque){
+            estoque -= quantidade;
+        }
+        else{
+            System.out.println("Estoque indisponível!");
+        }
+    }
+    public void adicionarEstoque(int adicionar){
+        if (adicionar > 0){
+            estoque =+ adicionar;
+        }
     }
 }
--- src/Usuarios.java
+++ src/Usuarios.java
@@ -26,4 +26,40 @@ public class Usuarios {
                 ",Telefone:" + telefone +
                 ",Tipo Usuario:" + tipoUsuario ;
     }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF(){
+        return CPF;
+    }
+    public void setCPF(String CPF){
+        this.CPF = CPF;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getSenha(){
+        return senha;
+    }
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(){
+        this.telefone = telefone;
+    }
 }
+
