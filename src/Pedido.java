d11a953181301f1bc111124cc3a72c62f5938b02
2222000271 <diasemillim2006@gmail.com>
2026-05-05 20:22:22 -0300

    Alterações pedido e restaurante

--- src/Pedido.java
+++ src/Pedido.java
@@ -3,24 +3,66 @@ import java.util.List;
 
 public class Pedido {
     private int idPedido;
+    private String status;
+
+    //AGREGAÇÃO
     private Cliente cliente;
     private Entregador entregador;
+    private Restaurante restaurante;
 
+    //ABSTRAÇÃO
+    private Pagamento pagamento;
+    private Entrega entrega;
+
+    //COMPOSIÇÃO
     private List<ItensPedido> itens = new ArrayList<>();
 
-    public Pedido(int idPedido, Cliente cliente){
+    public Pedido(int idPedido, Cliente cliente, String status, Entregador entregador,
+                  Restaurante restaurante, Pagamento pagamento, Entrega entrega){
+
         this.idPedido = idPedido;
         this.cliente = cliente;
+        this.status = "EM ABERTO";
+        this.entregador = entregador;
+        this.restaurante = restaurante;
+        this.pagamento = pagamento;
+        this.entrega = entrega;
     }
 
     public int getId(){
         return idPedido;
     }
 
-    public void adicionarItem(ItensPedido item){
-        itens.add(item);
+    public String getStatus(){
+        return status;
     }
-    public double calcaularTotal(){
+
+    public Cliente getCliente(){
+        return cliente;
+    }
+
+    public void adicionarItem(int idItensPedidos, Produtos produtos, int quantidade){
+
+        //Equals verifica se uma String daqui é igual de lá
+        if(!status.equals("EM ABERTO")){
+            System.out.println("Não é possivel incluir itens em um pedido finalizado");
+
+        }else{
+
+            //iniciando a contagem das id ItensProdutos a partir de 1
+            int idItensPedido= 1;
+
+            if(!itens.isEmpty()) {
+                idItensPedido = itens.size() + 1;
+
+                //adiciona o produto e quantidade instanciando um obj do itensPedido
+                ItensPedido item = new ItensPedido(idItensPedidos, produtos, quantidade);
+                itens.add(item);
+
+                System.out.println("Item adicionado no pedido");
+            }
+    }
+    public double getCalcaularTotal(){
         double total = 0;
         for(ItensPedido item : itens){
             total += item.subTotal();
